271f3c3346c3557d4ba8391db7c8cdbe68e646ff
Vinnie <sheepking4444@gmail.com>
2025-03-13 14:39:21 -0700

    card class, example data

---
+++ parse.py
@@ -0,0 +1,12 @@
+import csv
+
+class Card: 
+    def __init__(self, raw_row):
+        print(raw_row[0])
+
+
+with open('example_collection_2.csv', 'r') as csv_file:
+    reader = csv.reader(csv_file)
+    next(reader) # skip first row
+    for raw_row in reader:
+        card = Card(raw_row)
